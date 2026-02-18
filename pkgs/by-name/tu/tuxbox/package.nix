df4b4e5f23670cbc847083f6b81342c85209ad3c
Andreas Erdes <socialcoms@posteo.de>
2026-02-19 00:35:15 +0100

    tuxbox: 3.0.0 -> 3.0.1

--- pkgs/by-name/tu/tuxbox/package.nix
+++ pkgs/by-name/tu/tuxbox/package.nix
@@ -5,14 +5,14 @@
 }:
 python313Packages.buildPythonApplication (finalAttrs: {
   pname = "tuxbox";
-  version = "3.0.0";
+  version = "3.0.1";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "AndyCappDev";
     repo = "tuxbox";
-    rev = "5c4d27718f96ce511235a2e3e60b904d80722adb";
-    hash = "sha256-FryeqFbp7X5gJ0LCEDjFPgT8kmZ+E7pk5wp4/xPrr7Q=";
+    rev = "606a1604cf4e03c331ad034d8f60b263fc395fd7";
+    hash = "sha256-hBk4KhLNMgk8bFCZPQMtQlJ1/RB9qcL4kiF+eb3n4LU=";
   };
 
   build-system = [ python313Packages.setuptools ];
@@ -27,15 +27,16 @@ python313Packages.buildPythonApplication (finalAttrs: {
   patches = [
     # TuxBox uses a pgrep command to check if there is a running driver process.
     # However, the search argument does not work for NixOS - `python.*-m.*tuxbox`.
-    # We patch the command to use `tuxbox-wrapped` since that is the name the process will spawn as when using this package.
+    # Therefore, a patch is applied to use `tuxbox-wrapped` for the search since that is the name the process will spawn as when using this package.
     ./change-pgrep-command.patch
   ];
 
   meta = {
     changelog = "https://github.com/AndyCappDev/tuxbox/releases/tag/${finalAttrs.version}";
-    description = "Linux driver for all TourBox models - Native feel with USB, Bluetooth, haptics and graphical configuration GUI.";
+    description = "Linux driver for all TourBox models - Native feel with USB, Bluetooth, haptics and graphical configuration GUI";
     homepage = "https://github.com/AndyCappDev/tuxbox";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ CompileTime ];
+    mainProgram = "tuxbox";
   };
 })
